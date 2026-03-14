9e8dae75c323f6e1c95fad46c42bdfda51f9a973
Milankumar Vardaram Rawal <milanrawal1441@gmail.com>
2026-03-14 14:02:12 +0530

    admin_panel ui

--- coreinventory/accounts/urls.py
+++ coreinventory/accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
+    path('admin_panel/', views.admin_panel, name='admin_panel'),
 ]
--- coreinventory/accounts/views.py
+++ coreinventory/accounts/views.py
@@ -12,6 +12,10 @@ def index(request):
     warehouse_name = request.user.warehouse.name if request.user.is_authenticated else 'N/A'
     return render(request, 'home/index.html', {'warehouse_name': warehouse_name})
 
+def admin_panel(request):
+    warehouse_name = request.user.warehouse.name if request.user.is_authenticated else 'N/A'
+    return render(request, 'home/admin_panel.html', {'warehouse_name': warehouse_name})
+
 def user_login(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,6 +24,8 @@ def user_login(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'Login successful!')
+            if user.role == 1:
+                return redirect('admin_panel')
             return redirect('home')
         else:
             messages.error(request, 'Invalid username or password.')
