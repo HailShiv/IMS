216d8d9d7fe5479764ec863449482118c5332eca
Param-Sachani <paramsachani@gmail.com>
2026-03-14 13:43:26 +0530

    divam
    UI

--- coreinventory/accounts/urls.py
+++ coreinventory/accounts/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
-    path('logout/', views.user_logout, name='logout'),
+    # path('logout/', views.user_logout, name='logout'),
 ]
