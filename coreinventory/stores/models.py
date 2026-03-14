5cbcd7a949def1b38bd45290afd2095656b4f0bf
Divam Modi <Divammodi777@gmail.com>
2026-03-14 12:10:05 +0530

    login-registration

--- coreinventory/stores/models.py
+++ coreinventory/stores/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Warehouse(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
